f07c6d641562a5d690031bceeb7d1dad0c699010
PAK098 <usamaijaz2119gmail.com>
2024-10-06 17:37:46 +0500

    Path updated

--- Model/product.js
+++ Model/product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 //Schema
 const productSchema = new Schema({
-  id: { type: Number, required: true, unique: true },
+  id: { type: String, required: true, unique: true },
   title: {
     type: String,
     required: true,
--- controller/product.js
+++ controller/product.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 
-//importing product schema
+// Importing product schema
 const model = require("../Model/product");
 const Product = model.Product;
 
+// Create a new product
 exports.createProduct = async (req, res) => {
   try {
     const product = new Product(req.body);
@@ -11,57 +12,93 @@ exports.createProduct = async (req, res) => {
     res.status(201).json(savedProduct);
   } catch (err) {
     console.error(err);
-    res.status(500).json(err);
+    res.status(500).json({ message: err.message });
   }
 };
 
+// Get all products
 exports.getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find(); //    const products = await Product.find({price:{$gt:500}});
-
+    const products = await Product.find();
+    // Example for filtered results: const products = await Product.find({price: {$gt: 500}});
     res.json(products);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: err.message });
   }
 };
+
+// Get a single product by custom 'id' field
 exports.getProduct = async (req, res) => {
   try {
     const id = req.params.id;
-    const product = await Product.findById(id); //  const product =await Product.findById(mongoose.isObjectIdOrHexString(id))
+    const product = await Product.findOne({ id: id });
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
 
     res.json(product);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: err.message });
   }
 };
+
+// Replace an existing product by custom 'id' field
 exports.replaceProduct = async (req, res) => {
   try {
     const id = req.params.id;
-    const doc = await Product.findOneAndReplace({ _id: id }, req.body, {
-      new: true,
-    });
-    res.status(201).json(doc);
+    const updatedProduct = await Product.findOneAndReplace(
+      { id: id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(updatedProduct);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: err.message });
   }
 };
+
+// Update an existing product by custom 'id' field
 exports.updateProduct = async (req, res) => {
   try {
     const id = req.params.id;
-    const doc = await Product.findOneAndUpdate({ _id: id }, req.body, {
-      new: true,
-    });
-    res.status(201).json(doc);
+    const updatedProduct = await Product.findOneAndUpdate(
+      { id: id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(updatedProduct);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: err.message });
   }
 };
+
+// Delete a product by custom 'id' field
 exports.deleteProduct = async (req, res) => {
   try {
     const id = req.params.id;
-    const doc = await Product.findOneAndDelete({ id: id });
-    res.status(201).json(doc);
+    const deletedProduct = await Product.findOneAndDelete({ id: id });
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(deletedProduct);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json({ message: err.message });
   }
 };
